daf978c5ed19bb8174969759e162b2eae9a71243
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-02-06 15:54:23 +0530

    define functions for different mathematical operation

--- calculator.js
+++ calculator.js
@@ -1,5 +1,25 @@
 const userInput = document.querySelector(".input");
 
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return Math.round(a / b);
+}
+
+function reminder(a, b) {
+  return a % b;
+}
+
 userInput.addEventListener("click", (event) => {
   alert(event.target.textContent);
 });
