3e4a4f87c23ba34fc5034b1a7fe4b9a35fba7ae8
Vinay DG <vinay.dg@travancoreanalytics.com>
2025-02-06 16:52:33 +0530

    handle button clicks to be handled by handleNumbers and handleSymbols functions

--- calculator.js
+++ calculator.js
@@ -22,11 +22,24 @@ function changeSign(n) {
   return -1 * n;
 }
 
+function handleNumbers(n) {
+  currentNumber += n;
+  console.log(currentNumber);
+}
+function handleSymbols(s) {
+  currentOperator = s;
+  console.log(currentOperator);
+}
+
 function handleButtonClick(element) {
-  console.log(element.textContent);
+  if (isNaN(parseInt(element.textContent))) handleSymbols(element.textContent);
+  else handleNumbers(element.textContent);
 }
 
 const userInput = document.querySelector(".input");
+let currentNumber = "";
+let currentOperator = undefined;
+let previousNumber = "";
 
 userInput.addEventListener("click", (event) => {
   handleButtonClick(event.target);
