cc858dadcaf8a7eed808580fd22b48469af9ad64
Vinay DG <lavishinprince@gmail.com>
2025-02-06 20:30:49 +0530

    Feature/calculator functionality (#2)
    * detect button click by the user
    
    * define functions for different mathematical operation
    
    * add function to change the sign of number and refactor userInput
    variable
    
    * pass the click element to a funcction called handleButtonClick
    
    * handle button clicks to be handled by handleNumbers and handleSymbols
    functions
    
    * create a function to handle different operation
    
    * add functionality to the calculator. Need to refactor handleSymbols
    function
    
    * update the display whenever the user clicks a button
    
    * handle divide by 0 scenario
    
    * refactor some functionality from handleSymbols functions
    
    * removed console functions that were introduced for debugging
    
    * align text to right, add some padding and increase size for result and
    display divs'
    
    ---------
    
    Co-authored-by: Vinay DG <vinay.dg@travancoreanalytics.com>
    Co-authored-by: Vinay DG <vinaydg2001@gmail.com>

---
+++ calculator.js
@@ -0,0 +1,141 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return Math.round((a / b) * 100) / 100;
+}
+
+function reminder(a, b) {
+  return a % b;
+}
+
+function changeSign(n) {
+  return -1 * n;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+": {
+      return add(a, b);
+    }
+    case "-": {
+      return subtract(a, b);
+    }
+    case "×": {
+      return multiply(a, b);
+    }
+    case "÷": {
+      return divide(a, b);
+    }
+    case "%": {
+      return reminder(a, b);
+    }
+    case "±": {
+      return changeSign(parseInt(currentNumber));
+    }
+  }
+}
+
+function handleNumbers(n) {
+  if (parseInt(currentNumber) === 0) {
+    currentNumber = n;
+    return;
+  }
+  currentNumber += n;
+}
+
+function handleCalculation(s) {
+  previousNumber =
+    Math.round(
+      operate(
+        currentOperator,
+        parseFloat(previousNumber),
+        parseFloat(currentNumber),
+      ) * 100,
+    ) / 100;
+  previousNumber = previousNumber.toString();
+  if (s === "=") {
+    currentNumber = previousNumber;
+    previousNumber = "";
+    currentOperator = undefined;
+    return;
+  }
+  currentOperator = s;
+  currentNumber = "";
+}
+
+function handleDivideByZero() {
+  document.querySelector(".display").textContent = "";
+  document.querySelector(".result").textContent = "you cannot divide by 0";
+  calculatorPause = true;
+  currentNumber = "";
+  previousNumber = "";
+  currentOperator = undefined;
+}
+
+function handleSymbols(s) {
+  if (s === "C") {
+    currentOperator = undefined;
+    currentNumber = "";
+    previousNumber = "";
+  } else if (s === "⌫") {
+    if (currentNumber !== "")
+      currentNumber = currentNumber.slice(0, currentNumber.length - 1);
+  } else if (s === "±") {
+    if (currentNumber !== "") currentNumber = operate(s).toString();
+  } else if (s === ".") {
+    if (!currentNumber.includes(s)) currentNumber += s;
+    else if (currentNumber === "") currentNumber = "0" + s;
+  } else if (parseInt(currentNumber) === 0 && currentOperator === "÷") {
+    handleDivideByZero();
+  } else if (
+    currentNumber !== "" &&
+    previousNumber !== "" &&
+    currentNumber[currentNumber.length - 1] !== "."
+  ) {
+    handleCalculation(s);
+  } else if (currentNumber !== "" && s !== "=") {
+    previousNumber = currentNumber;
+    currentOperator = s;
+    currentNumber = "";
+  }
+}
+
+function displayUpdate() {
+  const display = document.querySelector(".display");
+  const result = document.querySelector(".result");
+  if (calculatorPause) {
+    calculatorPause = false;
+    return;
+  }
+  if (previousNumber !== "" && currentOperator !== undefined)
+    display.textContent = `${previousNumber} ${currentOperator}`;
+  else display.textContent = "";
+  result.textContent = `${currentNumber}`;
+}
+
+function handleButtonClick(element) {
+  displayUpdate();
+  if (isNaN(parseInt(element.textContent))) handleSymbols(element.textContent);
+  else handleNumbers(element.textContent);
+  displayUpdate();
+}
+
+const userInput = document.querySelector(".input");
+let currentNumber = "";
+let currentOperator = undefined;
+let previousNumber = "";
+let calculatorPause = false;
+
+userInput.addEventListener("click", (event) => {
+  handleButtonClick(event.target);
+});
