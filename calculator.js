5951a88750803ebb1c124d95b65ec1c05c530fba
Vinay DG <vinaydg2001@gmail.com>
2025-08-24 23:23:41 +0530

    small bug fix of equal not working

--- calculator.js
+++ calculator.js
@@ -99,8 +99,7 @@ function handleSymbols(s) {
   } else if (
     currentNumber !== "" &&
     previousNumber !== "" &&
-
-    currentNumber.endsWith(".")
+    (currentNumber.endsWith(".") || s == "=")
   ) {
     handleCalculation(s);
   } else if (currentNumber !== "" && s !== "=") {
